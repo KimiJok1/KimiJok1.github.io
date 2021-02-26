8ae0f8439aa73ed395ea30bc0a7b6d04e2c4810e
kimijok1 <kimi.jokelainen@edu.tampere.fi>
2021-02-26 08:14:38 +0200

    pain

--- samarium/index.php
+++ samarium/index.php
@@ -6,8 +6,8 @@
         }   
     }
     if (isset($_COOKIE["_LOGINMAIN"]) && $_COOKIE["_LOGINMAIN"] == true) {
-        include "./alku.php";
-        include "./navi.php";
+        include "./partials/alku.php";
+        include "./partials/navi.php";
 
         $sallitut = array("sqlharj1","demo1","demo2","demo3","demo4","demo4_lomakkeenkasittelija","demo5","demo6","demo7","demo8","demo9","demo10","demo11","demo12a","demo12b","demo13","harj1","harj2","harj3","harj4","harj5","harj6","harj7","harj8","harj9","harj10","harj11","harj12","harj13","harj14","harj15");
 
@@ -15,10 +15,10 @@
             $sivu = $_GET['sivu'];
             $kansio = $_GET['kansio'];
             if (in_array($sivu, $sallitut)) {
-                require "../$kansio/$sivu.php"; 
+                require "./$kansio/$sivu.php"; 
                 echo "<br><hr>";
                 echo "<h2> Lähdekoodi </h2>";
-                echo highlight_file("../$kansio/$sivu.php",1);
+                echo highlight_file("./$kansio/$sivu.php",1);
             }
         }
 
@@ -27,19 +27,19 @@
             $kansio = $_GET['kansio'];
             if (in_array($sivu, $sallitut)) {
                 echo "<h2> Lähdekoodi </h2>";
-                echo highlight_file("../$kansio/$sivu.xml",1);
+                echo highlight_file("./$kansio/$sivu.xml",1);
             }
         }
         
 
-        include "./loppu.php";
+        include "./partials/loppu.php";
     } else {
     ?>
         <head>
             <meta charset="utf-8">
             <title>PHP-harjoitukset</title>
-            <link rel="stylesheet" href="tyyli.css">
-            <link rel="icon" type="image/gif" href="./favicon.gif"/>
+            <link rel="stylesheet" href="./partials/tyyli.css">
+            <link rel="icon" type="image/gif" href="./partials/favicon.gif"/>
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         </head>
         <body>
